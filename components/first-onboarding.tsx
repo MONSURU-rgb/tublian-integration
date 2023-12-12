beddf90f111546e836daee5c223906d0fa8132ac
Afolabi Monsuru <monsuruafolabi463@gmail.com>
2023-12-12 13:57:03 +0100

    feat: Account setup section reponsiveness completed

--- components/first-onboarding.tsx
+++ components/first-onboarding.tsx
@@ -90,9 +90,9 @@ export function FirstOnboarding() {
           "flex-col",
           active < 3
             ? "w-1/2 px-10 lg:pl-[52px] lg:pr-0 md:pl-7 mobile:w-full sm:pl-4"
-            : "w-full pr-[103px] pl-[108px]"
+            : "w-full pr-[103px] pl-[108px] lg:pl-[52px] lg:pr-[52px] mobile:px-4"
         )}>
-        <nav className="flex gap-5 items-center justify-between sticky top-0 bg-[#121212] pt-68 lg:pt-10 lg:flex-col lg:gap-3 lg:items-start lg:pr-[39px] mobile:pt-5 mobile:pr-4 flex-wrap backdrop-opacity-90">
+        <nav className="flex gap-5 items-center justify-between sticky top-0 bg-[#121212] pt-68 lg:pt-10 lg:gap-3 lg:items-start mobile:pt-5 flex-wrap backdrop-opacity-90 z-50">
           <Image
             src="Tublian-logo.svg"
             alt="Tublian Logo"
@@ -139,74 +139,92 @@ export function FirstOnboarding() {
             nextStep={nextStep}
           />
         ) : active === 3 ? (
-          <div className="pt-10 flex items-center justify-center gap-10 flex-col">
-            <article className="flex flex-col gap-14 items-center">
-              <h2 className="text-4xl/144 font-bold text-base-900-dark font-grotesk">
-                How are you planning to use Tublian?
-              </h2>
+          <>
+            <div className="pt-10 flex gap-10 flex-col  items-center lg:pb-4 md:pb-10 flex-1">
+              <article className="flex flex-col gap-14 items-center">
+                <h2 className="text-4xl/144 font-bold text-base-900-dark font-grotesk text-center">
+                  How are you planning to use Tublian?
+                </h2>
 
-              <p className="text-base-800-dark font-grotesk text-base font-medium">
-                We will customize your experience based on your option.
-              </p>
-            </article>
-            <div className="flex gap-7">
-              {checkoutPageData.map((choice) => (
-                <section
-                  key={choice.id}
-                  className={clsx(
-                    "pr-14 pt-14 pb-5 pl-5 flex flex-col gap-18 justify-between",
-                    choice.id === checkedChoice ? "setUpWithGradient" : "setUp"
-                  )}
-                  onClick={() => setCheckedChoice(choice.id)}>
-                  <figure className="flex flex-col gap-2">
-                    {choice.id === checkedChoice ? (
-                      <Image
-                        src="/tick-circle.svg"
-                        alt="Account Set up Checked Icon"
-                        width={24}
-                        height={24}
-                        className="self-end"
-                      />
-                    ) : (
-                      <span
-                        className={clsx(
-                          "rounded-full bg-[#1E1E1E] border border-[#292929] self-end",
-                          choice.title === "Personal Projects"
-                            ? "w-5 h-[22px]"
-                            : "w-5 h-6"
-                        )}></span>
+                <p className="text-base-800-dark font-grotesk text-base font-medium lg:max-w-[379px] md:max-w-full text-center">
+                  We will customize your experience based on your option.
+                </p>
+              </article>
+              <div className="flex gap-7 mobile:flex-col mobile:w-full">
+                {checkoutPageData.map((choice) => (
+                  <section
+                    key={choice.id}
+                    className={clsx(
+                      "pr-14 pt-14 pb-5 pl-5 flex flex-col gap-18 justify-between mobile:flex-row",
+                      choice.id === checkedChoice
+                        ? "setUpWithGradient"
+                        : "setUp"
                     )}
-                    <Image
-                      src={choice.image}
-                      alt={`${choice.title}Image`}
-                      fill
-                      className="!relative object-cover"
-                    />
-                  </figure>
+                    onClick={() => setCheckedChoice(choice.id)}>
+                    <figure className="flex flex-col gap-2  mobile:flex-row-reverse w-full mobile:justify-between">
+                      {choice.id === checkedChoice ? (
+                        <Image
+                          src="/tick-circle.svg"
+                          alt="Account Set up Checked Icon"
+                          width={24}
+                          height={24}
+                          className="self-end mobile:self-center"
+                        />
+                      ) : (
+                        <span
+                          className={clsx(
+                            "rounded-full bg-[#1E1E1E] border border-[#292929] self-end mobile:self-center",
+                            choice.title === "Personal Projects"
+                              ? "w-5 h-[22px]"
+                              : "w-5 h-6 mobile:h-[22px]"
+                          )}></span>
+                      )}
 
-                  <article className="flex flex-col gap-[6px] items-center">
-                    <h3 className="font-grotesk text-2xl text-base-900-dark">
-                      {choice.title}
-                    </h3>
+                      <section className="flex">
+                        <Image
+                          src={choice.image}
+                          alt={`${choice.title}Image`}
+                          fill
+                          className="!relative object-contain mobile:max-w-[71px] z-30"
+                        />
 
-                    <p className="text-base-800-dark text-center font-grotesk text-base max-w-[210px]">
-                      {choice.description}
-                    </p>
-                  </article>
-                </section>
-              ))}
+                        <article className="mobile:flex flex-col gap-[6px] items-center hidden justify-start mobile:items-start">
+                          <h3 className="font-grotesk text-2xl text-base-900-dark mobile:text-lg/[30px]">
+                            {choice.title}
+                          </h3>
+
+                          <p className="text-base-800-dark text-start font-grotesk text-base max-w-[210px] mobile:text-sm">
+                            {choice.description}
+                          </p>
+                        </article>
+                      </section>
+                    </figure>
+
+                    <article className="flex flex-col gap-[6px] items-center mobile:hidden">
+                      <h3 className="font-grotesk text-2xl text-base-900-dark">
+                        {choice.title}
+                      </h3>
+
+                      <p className="text-base-800-dark text-start font-grotesk text-base max-w-[210px]">
+                        {choice.description}
+                      </p>
+                    </article>
+                  </section>
+                ))}
+              </div>
+              <button
+                className="action-button !justify-self-center h-fit"
+                onClick={nextStep}>
+                Next
+              </button>
             </div>
-            <button className="action-button" onClick={nextStep}>
-              Next
-            </button>
-            d
-            <footer className="pt-[62px] pb-[47px]">
+            <footer className="pt-[62px] pb-[47px] lg:pb-4 mx-auto md:pb-[40px] sm:hidden">
               <ul className="flex gap-5 font-grotesk text-base text-base-700-dark-tertiary">
                 <li>Privacy Policy</li>
                 <li>Terms</li>
               </ul>
             </footer>
-          </div>
+          </>
         ) : (
           <div className="pt-10 flex flex-col justify-center items-center gap-10">
             <article className="flex flex-col gap-14 items-center">
