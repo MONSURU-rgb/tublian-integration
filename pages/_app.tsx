837f704d592ad70ec775698646cf7614bfce5e45
Afolabi Monsuru <monsuruafolabi463@gmail.com>
2023-12-14 09:12:59 +0100

    fix: FIOU error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,23 @@ import { ActiveProvider } from "@/components";
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//       retry: false,
+//     },
+//   },
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.documentElement.style.opacity = "1";
+    document.documentElement.style.height = "100%";
+  }, []);
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <ActiveProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{opacity: 0}}>
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Sora:wght@100;200;300;400;500;600;700;800&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
